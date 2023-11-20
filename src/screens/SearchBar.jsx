0dd45b44cce7bc56154219d5124d3668c2d09a03
Yeganeh <vnus.daneshparvar@gmail.com>
2023-11-20 16:45:14 -0500

    commit

--- src/screens/SearchBar.jsx
+++ src/screens/SearchBar.jsx
@@ -4,6 +4,7 @@ import React, { useState} from 'react'
 import { View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native'
 
 export const SearchBar = ({onSearch}) => {
+  
     const [query, setQuery] = useState('');
     const [cuisine, setCuisine] = useState('');
     const [location, setLocation] = useState('');
